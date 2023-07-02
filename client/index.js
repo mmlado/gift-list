2d738db89857a6d30f01d9389e9747e7ded6f1f5
Mladen Milankovic <developer@mmlado.com>
2023-07-02 08:26:18 +0200

    Finish assignment.

--- client/index.js
+++ client/index.js
@@ -5,10 +5,12 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  const merkleTree = new MerkleTree(niceList);
+  const index = 0;
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    'proof': merkleTree.getProof(index),
+    'name': niceList[index],
   });
 
   console.log({ gift });
